3e8b9cf5b95efb3991db5157e4294bfdfc0e9351
Cameron Seamons <CameronSeamons@gmail.com>
2026-04-04 12:15:29 -0600

    made a more complicated 'HARD' version for day 3

---
+++ day3hard/letters.py
@@ -0,0 +1,73 @@
+""" Separated funcitons to have a cleaner main.py"""
+
+# Ask user to enter 3 random letters
+
+def letter_list():
+    letter_dict = {3: "first", 2: "second", 1: "third"}
+    letters = []
+    num = 3
+    while num > 0:
+        print("\nEnter letters to count in your text:")
+        letter = input(f"Enter the {letter_dict[num]} letter: ").lower()
+
+        # Validate: reject if a number
+        if letter.isnumeric():
+            print("Please enter a letter, not a number")
+            continue
+        # Validate: reject if more than 1 character
+        elif len(letter) > 1:
+            print("Please enter just ONE letter")
+            continue
+        # Validate: reject if empty
+        elif len(letter) == 0:
+            print("Please enter a letter")
+            continue
+        # Validate: reject if already in list
+        elif letter in letters:
+            print(f"Already used {letter}. Please enter a NEW letter")
+            continue
+        else:
+            # store each valid letter
+            letters.append(letter)
+            num -= 1
+    return letters
+
+# 1. How many times each of those letters appear
+def letter_count(letters, user_text):
+    # Store results
+    letter_count = {}
+    # If letter not found, store 0
+    for letter in letters:
+        letter_count[letter] = user_text.count(letter)
+
+    # Print results in format: Found [e] 1 times, [t] 2 times in your text 
+    letter_counts = ", ".join(f"[{letter}] {value} times" for letter,value in letter_count.items())
+        
+    return letter_counts
+
+def vowel_count(words):
+    # Count how many vowels (a, e, i, o, u) appear in the entire text
+    vowel_dict = {}
+    vowel_list = ["a", "e", "i", "o", "u"]
+    for word in words:
+        for letter in word:
+            if letter in vowel_list:
+                vowel_dict[letter] = word.count(letter)
+        
+    # Print results
+    
+    if len(vowel_dict) > 0:
+        vowel_counts = ", ".join(f"[{vowel}] {value} times" for vowel,value in vowel_dict.items())
+        results = f"Found {vowel_counts}"
+    else:
+        results = "No vowels in your text"
+        
+    return results
+
+# 3. First and last LETTERS of the entered text
+def first_last(user_text):
+    # (not first word, but first character)
+    first_letter = user_text[0][0]
+    last_letter = user_text[-1][-1]
+    
+    return first_letter, last_letter
---
+++ day3hard/words.py
@@ -0,0 +1,75 @@
+""" Separated funcitons to have a cleaner main.py"""
+
+
+# Split the text into a list of words
+def word_count(words):
+    word_count = len(words)
+    # Print the count
+    return word_count
+
+def reverse(words):
+    words.reverse()
+    reversed = " ".join(words)
+    
+    return reversed
+
+def find_python(words):
+    python = False
+    had_python = {True : "had", False : "did not have"}
+    if "python" in words:
+        python = True
+    
+    python_count = 0
+    for word in words:
+        if word == "python":
+            python_count += 1
+    if python_count > 0:
+        text = f"Your text {had_python[python]} Python in it [{python_count}] times."
+    else:
+        text = f"Your text {had_python[python]} Python in it"
+    
+    return text
+
+def most_common(words):
+    # Hint: you'll need to count occurrences of each word
+    # Print the word and how many times it appeared
+    word_counts = {}
+    for word in words:
+        word_counts[word] = words.count(word)
+    
+    highest_count = max(word_counts.values())
+    for word, value in word_counts.items():
+        if value == highest_count:
+            highest_word = word
+    
+    return highest_word, highest_count
+
+
+def unique_words(words):
+    # Hint: think about a data type that removes duplicates automatically
+    unique_words_set = set(words)
+    
+    unique_words_count = len(unique_words_set)
+    
+    return unique_words_count
+    
+def avg_length(words):
+    # Loop through the word list, get each length, calculate the average
+    lengths = []
+    for word in words:
+        lengths.append(len(word))
+        
+    avg_word_len = round(sum(lengths) / len(lengths),2)
+    
+    return avg_word_len
+    
+def title_case(words):
+    # Print what the text would look like in Title Case
+    # Do NOT use the built in title() method - try to do it manually
+    upper_words = []
+    for word in words:
+        new_word = word[0].upper() + word[1:]
+        
+        upper_words.append(new_word)
+    
+    return upper_words
