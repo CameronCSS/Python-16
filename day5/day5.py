a5daf45615744067dd1e0cdf1b1eb55cf33c676b
CameronCSS <cameronseamons@gmail.com>
2026-04-13 22:17:05 -0600

    added some day 5 practice

--- day5/day5.py
+++ day5/day5.py
@@ -1,4 +1,8 @@
 def unique_items():
+    '''
+    This function finds all the unique items in the list,
+    and returns them in the original order
+    '''
     # Cant use set because we need to keep original order
     items = [1,2,3,4,5,5,3,6,1,4,7]
     
@@ -12,6 +16,24 @@ def unique_items():
     # quick 1 liner
     print(list(dict.fromkeys(items)))
     
+def check_3_digits(number):
+    return number in range(100, 1000)
+
+def show_3_digits(numbers):
+    return [n for n in numbers if n in range(100, 1000)]
+
+def one_3_digits(numbers):
+    return any(n in range(100,1000) for n in numbers)
+
+def all_3_digits(numbers):
+    return all(n in range(100, 1000) for n in numbers)
+
+
+
     
 if __name__ == "__main__":
+    numbers = [145,675,76,789]
     unique_items()
+    print(show_3_digits(numbers))
+    print(one_3_digits(numbers))
+    print(all_3_digits(numbers))
