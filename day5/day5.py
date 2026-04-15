403080395c38216a5784a9d69cf216230e1e98d5
Cameron Seamons <CameronSeamons@gmail.com>
2026-04-15 15:58:32 -0600

    updated day5

--- day5/day5.py
+++ day5/day5.py
@@ -12,6 +12,20 @@ def unique_items():
     # quick 1 liner
     print(list(dict.fromkeys(items)))
     
+def most_expensive(products):
+    highest = 0
+    most = ''
+    for item, price in products:
+        if price > highest:
+            highest = price
+            most = item
+    return most
+
+def most_one_liner(products):
+    return max(products, key=lambda item: item[1])[0]
+
+products = ('coffee', 1.5), ('laptop', 500), ('muffin', 2.5), ('marshmallow', 1)
+            
     
 if __name__ == "__main__":
-    unique_items()
+    print(most_one_liner(products))
