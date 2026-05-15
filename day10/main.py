de4c1bff9129e3825cfe834b90b8046f50ce385a
Cameron Seamons <CameronSeamons@gmail.com>
2026-05-15 17:03:48 -0600

    Started coding day10 game

---
+++ day10/main.py
@@ -0,0 +1,13 @@
+"""
+Day 10 challenge is all about using pygame to make a game!
+This is going to be a real fun one!
+"""
+import pygame
+
+def main():
+    """
+    Main game logic will go here
+    """
+
+if __name__ == "__main__":
+    main()
