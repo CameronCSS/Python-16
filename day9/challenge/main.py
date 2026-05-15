f9c3c524d1a695550fb6c75be5fe850a5badad6c
Cameron Seamons <CameronSeamons@gmail.com>
2026-05-15 17:00:40 -0600

    Finished day 9, started day 10

--- day9/challenge/main.py
+++ day9/challenge/main.py
@@ -1,21 +1,48 @@
 import os
+import re
+import math
+import time
+from datetime import date
 
-cwd = os.getcwd()
+def main():
+    """
+    Searched through folder structure checking all txt files for defined serial number pattern
+    N[3 text chars]-[5 digits]
+    """
+    cwd = os.getcwd()
 
-path = f'{cwd}\My_Big_Directory'
+    path = os.path.join(cwd, 'My_Big_Directory')
 
-for folders, subfolders, files in os.walk(path):
-    # This line modifies list in place and ignores .git and pycache folders in our printout
-    subfolders[:] = [sub for sub in subfolders if sub not in ('.git', '__pycache__')]
+    pattern = r'N[a-zA-Z]{3}-\d{5}'
 
-    print(f'In folder: {folders}')
-    if subfolders:
-        print(f'Subfolders are: ')
-        for sub in subfolders:
-            if sub:
-                print(f'\t{sub}')
-    if files:
-        print('Files are: ')
+    results = []
+
+    start = time.time()
+
+    for folders, subfolders, files in os.walk(path):
+        subfolders[:] = [sub for sub in subfolders if sub not in ('.git', '__pycache__')]
         for file in files:
-            print(f'\t{file}')
-    print('\t')
+            # full path to the file
+            filepath = os.path.join(folders, file)
+            with open(filepath, 'r') as f:
+                # read the entire file
+                content = f.read()
+            # search for the pattern
+            match = re.search(pattern, content)
+            if match:
+                # store the result
+                results.append((file, match.group()))
+    end = time.time()
+    duration = math.ceil(end - start)
+
+    print(f"Search date: {date.today().strftime('%d/%m/%y')}")
+
+    print("FILE\t\tSERIAL NO.")
+    print("----\t\t----------")
+    for file, serial in results:
+        print(f"{file}\t{serial}")
+    print(f"\nNumbers found: {len(results)}")
+    print(f"Search duration: {duration} seconds")
+
+if __name__ == "__main__":
+    main()
